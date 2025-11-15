3b6febf61c2734dc89a8de69521fc9c8ddd8e626
Lxapk <73869778+Lxapk@users.noreply.github.com>
2025-11-15 15:22:03 +0800

    更新 uninstall.sh

--- OpenList-magisk/uninstall.sh
+++ OpenList-magisk/uninstall.sh
@@ -4,6 +4,7 @@ MODDIR="/data/adb/modules/OpenList"
 
 pkill -9 -f 'openlist server' 2>/dev/null
 pkill -9 -f 'control.sh' 2>/dev/null
+pkill -9 -f 'service.sh' 2>/dev/null
 
 if [ -e /sys/power/wake_lock ]; then
   echo "OpenList_Service" >/sys/power/wake_unlock 2>/dev/null || true
